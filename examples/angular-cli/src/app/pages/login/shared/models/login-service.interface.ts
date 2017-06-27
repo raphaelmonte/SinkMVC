b2a933cd3aef2d271fe02b8564dd7dd7018f481f
Raphael Monte <raphaelmontesantos@gmail.com>
2017-06-26 22:33:51 -0300

    Remove Example

--- examples/angular-cli/src/app/pages/login/shared/models/login-service.interface.ts
+++
@@ -1,8 +0,0 @@
-import {Observable} from "rxjs/Rx";
-import {User} from "../../../../shared/models/user/user.model";
-
-export interface ILoginService {
-    
-    login(user: User): Observable<Object>;
-    
-}
--- examples/angular-cli/src/app/pages/login/shared/notifications/login.notification.ts
+++
@@ -1,7 +0,0 @@
-export class LoginNotifications {
-
-    public static LOGIN: string = "LoginNotifications.LOGIN";
-    public static SUCCESS_LOGIN: string = "LoginNotifications.SUCCESS_LOGIN";
-    public static FAILURE_LOGIN: string = "LoginNotifications.FAILURE_LOGIN";
-
-}
