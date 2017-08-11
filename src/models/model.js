da2c02068ada17c986a72079a22b1d92496dd355
admin <admin@midias-12.localdomain>
2017-08-11 10:32:32 -0300

    fix typescript command build

---
+++ src/models/model.js
@@ -0,0 +1,15 @@
+"use strict";
+var Model = (function () {
+    function Model() {
+    }
+    Model.prototype.set = function (attrs) {
+        for (var key in attrs) {
+            this[key] = attrs[key];
+        }
+    };
+    Model.prototype.get = function (attr) {
+        return this[attr];
+    };
+    return Model;
+}());
+exports.Model = Model;
